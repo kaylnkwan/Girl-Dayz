6b48ab02e1bf1a428bf04bf6178412ec77b6acfc
Kayln Kwan <kkwan@branksome.on.ca>
2020-04-21 11:29:48 -0400

    Initial Commit

---
+++ Girl Dayz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Girl Dayz
+//
+//  Created by KKwan on 2020-04-21.
+//  Copyright © 2020 GirlDayz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
